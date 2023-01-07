c3bad19196b288ddac1613dffc3db15fda0aafb8
Marta <maardiakiv@gmail.com>
2023-01-07 21:48:29 +0100

    add Home page base layout and logic

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import { fetchTrending } from 'utils/api';
+import { MovieList } from 'components/MovieList.jsx/MovieList';
+
+export function Home() {
+  const [trending, setTrending] = useState([]);
+
+  useEffect(() => {
+    async function getTrending() {
+      try {
+        const response = await fetchTrending();
+        setTrending(response.data.results);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    getTrending();
+  }, []);
+
+  return (
+    <main>
+      <MovieList movies={trending} />
+    </main>
+  );
+}

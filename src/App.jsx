04d4dc719a88f3f8f009343946531cb791db6751
Marta <maardiakiv@gmail.com>
2023-01-07 20:19:43 +0100

    routes hierarchy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 // import { Section } from 'components/Section/Section';
 import { Link } from './App.styled';
+import { Home } from 'pages/Home';
+import { Movies } from 'pages/Movies';
+import { MovieDetails } from 'pages/MovieDetails';
 
 export function App() {
   return (
@@ -13,6 +16,17 @@ export function App() {
           <Link to="/movies">Movies</Link>
         </nav>
       </header>
+      <Routes>
+        <Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            {/* TODO  Change element accordingly !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
+            <Route path="cast" element={<MovieDetails />} />
+            <Route path="rewievs" element={<MovieDetails />} />
+          </Route>
+        </Route>
+      </Routes>
     </>
   );
 }

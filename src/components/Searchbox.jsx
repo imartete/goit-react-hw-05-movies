aff445fa321a99b2b333b72e7a27828ba2c5abe3
Marta <maardiakiv@gmail.com>
2023-01-09 19:47:31 +0100

    add Searchbox (form) for searching by a query

---
+++ src/components/Searchbox.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export function SearchBox({ onSubmit }) {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  function handleChange(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (searchQuery.trim() === '') {
+      alert('Please enter the name of the movie');
+      return;
+    }
+    onSubmit(searchQuery);
+    setSearchQuery('');
+    event.currentTarget.reset();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="input" type="text" onInput={handleChange} />
+      <button type="submit">Search</button>
+    </form>
+  );
+}

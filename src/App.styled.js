d8b86c2e44091de8bb4f4db186586af0fbf97dce
Marta <maardiakiv@gmail.com>
2023-01-09 18:59:06 +0100

    add more styled components

--- src/App.styled.js
+++ src/App.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
-export const Link = styled(NavLink)`
+export const HeaderLink = styled(NavLink)`
   padding: 8px 16px;
   border-radius: 4px;
   text-decoration: none;
@@ -25,4 +25,34 @@ export const Section = styled.div`
   width: 100%;
   height: 100%;
   font-size: 18px;
+`;
+
+export const GoBack = styled(Link)`
+  display: block;
+  width: 100px;
+  padding: 8px 16px;
+  border-radius: 4px;
+  text-decoration: none;
+  color: white;
+  font-weight: 500;
+  background-color: grey;
+`;
+
+export const CardMeta = styled.div`
+  display: flex;
+  gap: 30px;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  gap: 20px;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-evenly;
+  list-style: none;
+  padding: 0;
+`;
+
+export const ListItem = styled.li`
+  width: 210px;
 `;
